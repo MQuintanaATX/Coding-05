4b04c352dd282864ffca282ff4848f905139ee9c
MQuintanaATX <33012542+MQuintanaATX@users.noreply.github.com>
2019-03-31 08:56:49 -0500

    Added the Node structure

--- Hashing.h
+++ Hashing.h
@@ -8,4 +8,17 @@ Notes: Servers as the interface for the main Hashing table
 #ifndef CODING_05_HASHING_H
 #define CODING_05_HASHING_H
 
+#include <string>
+
+using namespace std;
+/*
+ * Structure Definition
+ */
+struct Node {
+    int id;
+    string data;
+    Node *forward;
+};
+
+
 #endif //CODING_05_HASHING_H
