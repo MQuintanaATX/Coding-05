c01863b9480a300a99432ee00c6f578bc7a4731b
MQuintanaATX <33012542+MQuintanaATX@users.noreply.github.com>
2019-03-31 09:34:16 -0500

    isEmpty sucessfully tested

--- Hashing.cpp
+++ Hashing.cpp
@@ -4,10 +4,9 @@ Assignment: Coding 05 - Hash Tables
 Purpose: Implementation - Hashing
 Notes:   Implementation file for the hash table class
 ************************************************************************ */
-
 #include "Hashing.h"
 
-bool Hashing::isEmpty() { //checks to see if the hash table is empt
+bool Hashing::isEmpty() { //checks to see if the hash table is empty
     for (int i; i <=20; i++) {
         if (hashtable[i] != nullptr){
             return false;
